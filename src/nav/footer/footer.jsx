3996b12dceba86d501d45acfd9a23d9f7fe7d658
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-06 20:57:26 +0300

    Created footer component <react>

---
+++ src/nav/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../footer/footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer class="footer">
+                <div class="footer-content">
+                    <div class="logo"><h1>Contact Manager</h1></div>
+                    <p>&copy; Contact Manager. All rights reserved.</p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
